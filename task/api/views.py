fe5a2302c1b5527e7b5f5d3a18369a0e2ca736a9
Mosihere <Mostafakhoshhal@yahoo.com>
2024-10-13 22:38:26 +0330

    TaskViewSet implemented

---
+++ task/api/views.py
@@ -0,0 +1,9 @@
+from task.models import Task
+from rest_framework.viewsets import ModelViewSet
+from .serializers import TaskSerializer
+
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
