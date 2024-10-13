4cd82914b5f878a4ffd473136e46db6dc0d01a28
Mosihere <Mostafakhoshhal@yahoo.com>
2024-10-14 01:44:10 +0330

    adding a ForeignKey to user

--- task/models.py
+++ task/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
+
+User = get_user_model()
 
 class Task(models.Model):
     BACKLOG_STATUS = 'B'
@@ -17,6 +20,7 @@ class Task(models.Model):
     status = models.CharField(max_length=1, choices=TASK_STATUSES_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.title
