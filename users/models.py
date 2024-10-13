93ae4e4c2a516aa394dbe5cc2ba0e8518cee7621
Mosihere <Mostafakhoshhal@yahoo.com>
2024-10-13 23:54:16 +0330

    extend BaseUserManager user_creation to get phone_number instead of username

--- users/models.py
+++ users/models.py
@@ -1,7 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError('The Phone Number must be set')
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(phone_number, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=12, unique=True)
+    username = None
+    email = None
+    phone_number = models.CharField(unique=True, max_length=11)
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.phone_number
+    
+
