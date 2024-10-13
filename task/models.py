a63ace43a616a418d7ad3b33c158569029d2cb21
Mosihere <Mostafakhoshhal@yahoo.com>
2024-10-13 22:07:22 +0330

    task model implemented

--- task/models.py
+++ task/models.py
@@ -2,3 +2,21 @@ from django.db import models
 
 
 
+class Task(models.Model):
+    BACKLOG_STATUS = 'B'
+    IN_PROGRESS_STATUS = 'I'
+    DONE_STATUS = 'D'
+
+    TASK_STATUSES_CHOICES = (
+        (BACKLOG_STATUS, 'Backlog'),
+        (IN_PROGRESS_STATUS, 'In Progress'),
+        (DONE_STATUS, 'Done')
+    )
+
+    title = models.CharField(max_length=255)
+    status = models.CharField(max_length=1, choices=TASK_STATUSES_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
