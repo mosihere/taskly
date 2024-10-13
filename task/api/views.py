563338b4776d1e736901e6b6a949b142aa8c22a0
Mosihere <Mostafakhoshhal@yahoo.com>
2024-10-14 01:42:19 +0330

    applying permission_class and modify get_queryset to show different tasks based on UserType

--- task/api/views.py
+++ task/api/views.py
@@ -1,9 +1,17 @@
 from task.models import Task
 from rest_framework.viewsets import ModelViewSet
 from .serializers import TaskSerializer
+from .permissions import IsOwnerOrAdmin
 
 
 
 class TaskViewSet(ModelViewSet):
-    queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsOwnerOrAdmin]
+
+    def get_queryset(self):
+
+        if self.request.user.is_staff:
+            return Task.objects.select_related('user').all()
+        
+        return Task.objects.select_related('user').filter(user=self.request.user)
