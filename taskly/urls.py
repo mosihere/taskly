4c782004117f5f65ad7e2dd57d3f98974d7a1fa2
Mosihere <Mostafakhoshhal@yahoo.com>
2024-10-13 22:27:55 +0330

    all paths prefixed with api and task routes added aswell

--- taskly/urls.py
+++ taskly/urls.py
@@ -7,6 +7,7 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('api/task/', include('task.api.urls')),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.jwt')),
 ] + debug_toolbar_urls()
