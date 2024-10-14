ad815233a45f3fc4562c65c696f469dc55a74533
Mosihere <Mostafakhoshhal@yahoo.com>
2024-10-14 18:23:05 +0330

    Add Swagger and Redoc API documentation with drf-yasg

--- task/api/urls.py
+++ task/api/urls.py
@@ -1,9 +1,30 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 from .views import TaskViewSet
 
-
-
+# Define the router and register the TaskViewSet
 router = DefaultRouter()
 router.register('tasks', TaskViewSet, basename='task')
 
-urlpatterns = router.urls
+# Set up the schema view for Swagger and Redoc
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Task Management API",
+        default_version='v1',
+        description="API documentation for the Taskly",
+    ),
+    public=True,
+    permission_classes=(permissions.IsAuthenticated,),
+)
+
+# Combine the API routes with the documentation routes
+urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
+# Include the router's routes
+urlpatterns += router.urls
--- taskly/settings.py
+++ taskly/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'drf_yasg',
     'debug_toolbar',
     'rest_framework',
     'djoser',
