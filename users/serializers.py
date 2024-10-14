064c79f3eac30dd61dfefd39b7d10f4d12293934
Mosihere <Mostafakhoshhal@yahoo.com>
2024-10-14 16:46:56 +0330

    UserDeleteSerializer and UserCreateSerializer customized by extending djoser serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,35 +1,36 @@
-from djoser.serializers import UserSerializer as BaseUserSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseUserCreateSerializer
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.exceptions import ValidationError
 
 
 
 User = get_user_model()
 
 class UserSerializer(BaseUserSerializer):
-
-    phone_number = serializers.CharField(required=True)
-
     class Meta(BaseUserSerializer.Meta):
-        fields = ['phone_number', 'password']
+        fields = ['id', 'phone_number', 'first_name', 'last_name']
+    
 
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['id', 'phone_number', 'password', 'first_name', 'last_name']
+        
         extra_kwargs = {
             'password': {'write_only': True}
         }
-    
+
     def validate_phone_number(self, value):
         if not value.isdigit() or len(value) != 11:
             raise serializers.ValidationError('Phone number must be 11 digits long and contain only numbers.')
         
-        if User.objects.filter(phone_number=value).exists():
-            raise ValidationError('A user with this phone number already exists.')
-        
         return value
 
-    def create(self, validated_data):
-        user_password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(user_password)
-        user.save()
-        return user
+
+from djoser.serializers import UserDeleteSerializer as BaseUserDeleteSerializer
+
+class UserDeleteSerializer(serializers.Serializer):
+    """
+    Serializer to allow deletion of a user without requiring a current password.
+    This can be left empty to prevent deletion for regular users.
+    """
+    pass
